b6e083912dd968082e254b2b9947494b3285e0d6
ben <bholsinger@gmail.com>
2025-09-24 15:33:43 -0600

    refactor: Simplify header logo to just 'BH' in circle for cleaner design

--- src/components/SimpleHeader.tsx
+++ src/components/SimpleHeader.tsx
@@ -54,10 +54,9 @@ const SimpleHeader = () => {
         <div className="flex items-center justify-between">
           {/* Logo */}
           <Link href="/" className="flex items-center space-x-2">
-            <div className="w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center">
-              <span className="text-white font-bold text-sm">BH</span>
+            <div className="w-10 h-10 bg-blue-600 rounded-full flex items-center justify-center">
+              <span className="text-white font-bold text-lg">BH</span>
             </div>
-            <span className="font-bold text-lg">Ben H.</span>
           </Link>
 
           {/* Desktop Navigation */}
